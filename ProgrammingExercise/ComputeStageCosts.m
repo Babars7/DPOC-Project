dae043ea59698993c54fe38a6304ed8a66650699
fmartinmonier <74377209+fmartinmonier@users.noreply.github.com>
2020-12-06 14:56:35 +0100

    Add files via upload

--- ProgrammingExercise/ComputeStageCosts.m
+++ ProgrammingExercise/ComputeStageCosts.m
@@ -28,6 +28,64 @@ function G = ComputeStageCosts(stateSpace, map)
     global NORTH SOUTH EAST WEST HOVER
     global K
     global TERMINAL_STATE_INDEX
+    
+    P = ComputeTransitionProbabilities_2(stateSpace, map);
+    G = zeros(K,5);
+    INPUTS = [NORTH, SOUTH, EAST, WEST, HOVER]; 
+    
+    for stateSpace_i = 1:K
+        m_i = stateSpace(stateSpace_i,1);
+        n_i = stateSpace(stateSpace_i,2);
+        payload = stateSpace(stateSpace_i,3); %0 is no payload, 1 is payload
+        
+    %terminal state cost
+    if (stateSpace_i == TERMINAL_STATE_INDEX)
+        G(stateSpace_i,:) = 0;
+        continue
+    end
+    
+    %taking care of cases where arrival j is a tree or outside the map. inf represents an impossible input
+        for l = 1:length(INPUTS)
+            switch l
+                case NORTH
+                    if (n_i + 1 > size(map,2) || map(m_i, n_i + 1) == TREE)
+                        G(stateSpace_i,l) = inf;
+                        continue
+                    end
+                case SOUTH
+                    if (n_i - 1 < 1 || map(m_i, n_i - 1) == TREE)
+                        G(stateSpace_i,l) = inf;
+                        continue
+                    end
+                case EAST
+                    if (m_i + 1 > size(map,1) || map(m_i + 1, n_i) == TREE)
+                        G(stateSpace_i,l) = inf;
+                        continue
+                    end
+                case WEST
+                    if (m_i - 1 < 1 || map(m_i - 1, n_i) == TREE)
+                        G(stateSpace_i,l) = inf;
+                        continue
+                    end   
+            end
+        end
+    
+        for l = 1:length(INPUTS)
+                for stateSpace_j = 1:K
 
-end
+                    if stateSpace_j == BASE
+                        G(stateSpace_i,l) = G(stateSpace_i,l) + Nc * P(stateSpace_i,stateSpace_j,l)*G(;
+                    else
+                        G(stateSpace_i,l) = G(stateSpace_i,l) + P(stateSpace_i,stateSpace_j,l);
+                    end
+
+                end
+        end
+    
+
+    end
+
+
+
+
 
