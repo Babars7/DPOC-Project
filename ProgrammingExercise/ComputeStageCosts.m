6ce4747752afc02e68a16b93ed29a8d88d87f7fe
fmartinmonier <74377209+fmartinmonier@users.noreply.github.com>
2020-12-08 16:48:04 +0100

    Add files via upload

--- ProgrammingExercise/ComputeStageCosts.m
+++ ProgrammingExercise/ComputeStageCosts.m
@@ -30,55 +30,60 @@ function G = ComputeStageCosts(stateSpace, map)
     global TERMINAL_STATE_INDEX
     
     P = ComputeTransitionProbabilities(stateSpace, map);
-    G = zeros(K,5);
     INPUTS = [NORTH, SOUTH, EAST, WEST, HOVER]; 
+    G = zeros(K,length(INPUTS));
+    
+    [m_base, n_base] = find(map == BASE);
+    base_stateSpace = find(ismember(stateSpace, [m_base, n_base, 0], 'rows'));
     
     for stateSpace_i = 1:K
         m_i = stateSpace(stateSpace_i,1);
         n_i = stateSpace(stateSpace_i,2);
-        payload = stateSpace(stateSpace_i,3); %0 is no payload, 1 is payload
         
-    %terminal state cost
-    if (stateSpace_i == TERMINAL_STATE_INDEX)
-        G(stateSpace_i,:) = 0;
-        continue
-    end
+        %terminal state cost
+        if (stateSpace_i == TERMINAL_STATE_INDEX)
+            G(stateSpace_i,:) = 0;
+            continue
+        end
     
-    %taking care of cases where arrival j is a tree or outside the map. inf represents an impossible input
-        for l = 1:length(INPUTS)
-                if l == NORTH
+        %taking care of cases where arrival j is a tree or outside the map. inf represents an impossible input
+        for input = 1:length(INPUTS)
+                if input == NORTH
                     if (n_i + 1 > size(map,2) || map(m_i, n_i + 1) == TREE)
-                        G(stateSpace_i,l) = inf;
+                        G(stateSpace_i,input) = inf;
                         continue
                     end
-                elseif l == SOUTH
+                elseif input == SOUTH
                     if (n_i - 1 < 1 || map(m_i, n_i - 1) == TREE)
-                        G(stateSpace_i,l) = inf;
+                        G(stateSpace_i,input) = inf;
                         continue
                     end
-                elseif l == EAST
+                elseif input == EAST
                     if (m_i + 1 > size(map,1) || map(m_i + 1, n_i) == TREE)
-                        G(stateSpace_i,l) = inf;
+                        G(stateSpace_i,input) = inf;
                         continue
                     end
-                elseif l == WEST
+                elseif input == WEST
                     if (m_i - 1 < 1 || map(m_i - 1, n_i) == TREE)
-                        G(stateSpace_i,l) = inf;
+                        G(stateSpace_i,input) = inf;
                         continue
                     end   
-                end 
-    
+                end
+                
                 for stateSpace_j = 1:K
 
-                    if stateSpace_j == BASE
-                        G(stateSpace_i,l) = G(stateSpace_i,l) + Nc * P(stateSpace_i,stateSpace_j,l);
+                    if stateSpace_j == base_stateSpace
+                        G(stateSpace_i,input) = G(stateSpace_i,input) + Nc * P(stateSpace_i,stateSpace_j,input);
                     else
-                        G(stateSpace_i,l) = G(stateSpace_i,l) + P(stateSpace_i,stateSpace_j,l);
+                        G(stateSpace_i,input) = G(stateSpace_i,input) + P(stateSpace_i,stateSpace_j,input);
                     end
 
                 end
+
+
         end
     end
+end
 
 
 
